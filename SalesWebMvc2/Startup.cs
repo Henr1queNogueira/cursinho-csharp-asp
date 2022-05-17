bdb0775327769fbd850e856edd9ab93b1cdd7673
Henrique Nogueira <hsantosnogueira@gmail.com>
2022-05-16 20:25:34 -0400

    Adaptação para MySQL e primeira migration

--- SalesWebMvc2/Startup.cs
+++ SalesWebMvc2/Startup.cs
@@ -36,8 +36,10 @@ namespace SalesWebMvc2
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            //conexao com o banco de dados
             services.AddDbContext<SalesWebMvc2Context>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvc2Context")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvc2Context"), builder =>
+                        builder.MigrationsAssembly("SalesWebMvc2")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
